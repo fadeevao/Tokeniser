b4a7b1e2c8d9a1001c3016e864fdf60bbaec69c8
ofadejeva <olgafadejeva11@gmail.com>
2016-03-31 22:22:40 +0100

    Added simple unit tests

--- main/src/tokeniser.py
+++ main/src/tokeniser.py
@@ -1,22 +1,44 @@
 import re
+import unittest
 
 def splitBySpaceAndPunctuation(line) :
     punctuationMarks = r'[.?\-",!():;]'
-    foundPunctuation = set(re.findall(punctuationMarks, line))
+    specialSymbols = r'[$£%^&*¬+]'
     for index,item in enumerate(line):
-        if re.search(punctuationMarks,item) :
-            if checkItemsAreNotDigits(line[index-1], item) :
-                    line = line[:index] + " " + line[index:]
+        if re.search(specialSymbols, item):
+            line = insertSpaceIntoStringAfterItemAtSomeIndex(line, index)
 
+        if re.search(punctuationMarks,item) :
+            if bothItemsAreNotDigits(line[index-1], item) :
+                line = insertSpaceIntoStringAfterItemAtSomeIndex(line, index)
     print(line)
     return line.split()
 
-def checkItemsAreNotDigits(prev, current) :
+def bothItemsAreNotDigits(prev, current) :
     digitRegex = '\d'
     if prev != " ":
         if not re.match(digitRegex, current) and not re.match(digitRegex, prev):
             return True
     return False
 
+def insertSpaceIntoStringAfterItemAtSomeIndex(line, index) :
+    return line[:index+1] + " " + line[index+1:]
 
-splitBySpaceAndPunctuation("This costs 5.55 dollars!")
+splitBySpaceAndPunctuation("nh %5.55 dollars!")
+
+class TestTokeniser(unittest.TestCase):
+
+    def testCheckItemsAreNotDigits(self):
+        self.assertTrue(bothItemsAreNotDigits('a', 'b'))
+        self.assertFalse(bothItemsAreNotDigits('a', '1'))
+        self.assertFalse(bothItemsAreNotDigits('2', '1'))
+
+    def testInsertSpaceIntoStringAfterSomeIndex(self):
+        self.assertEqual(insertSpaceIntoStringAfterItemAtSomeIndex("hello", 0), "h ello")
+        self.assertEqual(insertSpaceIntoStringAfterItemAtSomeIndex("hello", 1), "he llo")
+        self.assertEqual(insertSpaceIntoStringAfterItemAtSomeIndex("hello", 2), "hel lo")
+        self.assertEqual(insertSpaceIntoStringAfterItemAtSomeIndex("hello", 10), "hello ")
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestTokeniser)
+unittest.TextTestRunner().run(suite)
