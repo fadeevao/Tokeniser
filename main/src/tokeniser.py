2c7058947d0752ee77d9328f9bf33ba7ad049965
ofadejeva <olgafadejeva11@gmail.com>
2016-03-31 21:14:18 +0100

    Small refactor and added a function

--- main/src/tokeniser.py
+++ main/src/tokeniser.py
@@ -3,15 +3,20 @@ import re
 def splitBySpaceAndPunctuation(line) :
     punctuationMarks = r'[.?\-",!():;]'
     foundPunctuation = set(re.findall(punctuationMarks, line))
-    print(foundPunctuation)
     for index,item in enumerate(line):
-        for punctuation in foundPunctuation :
-            if item == punctuation and line[index-1] != " " :
-                if not re.match('\d', item) and not re.match('\d',  line[index-1]) :
+        if re.search(punctuationMarks,item) :
+            if checkItemsAreNotDigits(line[index-1], item) :
                     line = line[:index] + " " + line[index:]
 
     print(line)
     return line.split()
+
+def checkItemsAreNotDigits(prev, current) :
+    digitRegex = '\d'
+    if prev != " ":
+        if not re.match(digitRegex, current) and not re.match(digitRegex, prev):
+            return True
+    return False
 
 
 splitBySpaceAndPunctuation("This costs 5.55 dollars!")
