d1c6b9d0a23968386b4ae9dde6d6c7f9ba962a36
ofadejeva <olgafadejeva11@gmail.com>
2016-04-11 20:05:49 +0100

    Starting work with apostrophes and tests

--- main/src/tokeniser.py
+++ main/src/tokeniser.py
@@ -5,6 +5,8 @@ def splitBySpaceAndPunctuation(line) :
     #regular expressions
     specialSymbols = r'[.?\-",!():;$£%^&*¬+]'
     alphanumericalCharacters= r'[a-zA-Z0-9]'
+    apostrophe = r'[\']'
+    capitalLetters = r'[A-Z]'
 
     spacesInserted = 0 #for indexing reasons
     lineCopy = line #copy of the original piece of text, needed for indexing reasons
@@ -16,10 +18,19 @@ def splitBySpaceAndPunctuation(line) :
                 lineCopy = insertSpaceIntoStringAfterItemAtSomeIndex(lineCopy, index+spacesInserted)
                 spacesInserted = spacesInserted + 1
 
-        if re.search(alphanumericalCharacters, item) :
+        if re.search(alphanumericalCharacters, item) and index != len(line) - 1:
             if re.search(specialSymbols, line[index+1]) : #if the next symbol after the capital letter is a special symbol
                 lineCopy = insertSpaceIntoStringAfterItemAtSomeIndex(lineCopy, index + spacesInserted)
                 spacesInserted = spacesInserted + 1
+
+        #deal with last names and other cases where pattern is like [A-Z'A-Z]
+        if re.search(apostrophe, item) :
+            if index != 0 and index != len(line) -1 :
+                if re.search(capitalLetters, line[index-1]) and re.search(capitalLetters, line[index+1]) :
+                    lineCopy = insertSpaceIntoStringAfterItemAtSomeIndex(lineCopy,index+spacesInserted)
+                    lineCopy = insertSpaceIntoStringAfterItemAtSomeIndex(lineCopy, index-1 + spacesInserted)
+                    spacesInserted = spacesInserted +2
+
 
     print(lineCopy)
     return lineCopy.split()
@@ -34,7 +45,7 @@ def bothItemsAreNotDigits(prev, current) :
 def insertSpaceIntoStringAfterItemAtSomeIndex(line, index) :
     return line[:index+1] + " " + line[index+1:]
 
-splitBySpaceAndPunctuation("Hello from the other side 45.")
+splitBySpaceAndPunctuation("O'Neill")
 
 class TestTokeniser(unittest.TestCase):
 
@@ -53,6 +64,7 @@ class TestTokeniser(unittest.TestCase):
         self.assertEqual(splitBySpaceAndPunctuation("..."), '. . . '.split())
         self.assertEqual(splitBySpaceAndPunctuation("Hello world."), 'Hello world .'.split())
         self.assertEqual(splitBySpaceAndPunctuation("J. Smith said that..."), 'J . Smith said that . . . '.split())
+        self.assertEqual(splitBySpaceAndPunctuation("O'Neill"), 'O \' Neill'.split())
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestTokeniser)
 unittest.TextTestRunner().run(suite)
